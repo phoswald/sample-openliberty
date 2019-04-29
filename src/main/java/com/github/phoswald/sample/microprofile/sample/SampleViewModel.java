9117474dcf6940e7eb0d0a30e6e9a7cc0c5162f2
Philip Oswald <philip.oswald@gmail.com>
2019-04-29 21:53:45 +0200

    Cleanup, use string from properties

--- src/main/java/com/github/phoswald/sample/microprofile/sample/SampleViewModel.java
+++ src/main/java/com/github/phoswald/sample/microprofile/sample/SampleViewModel.java
@@ -3,11 +3,12 @@ package com.github.phoswald.sample.microprofile.sample;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class SampleViewModel {
 
     public String greeting = "Hello, World!";
     public String now = ZonedDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
-    public Map<String, String> env = System.getenv();
-    public Map<Object, Object> props = System.getProperties();
+    public Map<String, String> env = new TreeMap<>(System.getenv());
+    public Map<Object, Object> props = new TreeMap<>(System.getProperties());
 }
